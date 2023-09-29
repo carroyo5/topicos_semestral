9f97b84530373b839a7867962a9dd77be25cd515
Cristhian Arroyo <arroyocristhian8@gmail.com>
2023-09-28 19:19:06 -0500

    Se añade pantalla secundaria para la cámara y se actualiza la pantalla principal

---
+++ InitialScreen.py
@@ -0,0 +1,77 @@
+import tkinter as tk
+from tkinter import ttk
+from styles import get_app_style
+import os
+from tkinter import filedialog
+from app import CameraScreen
+import _tkinter
+
+class InitialScreen(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        #Cofiguracion de la pantalla
+        self.title('Pantalla de Inicio')
+        self.geometry('520x520')
+        self.resizable(width=False, height=False)
+        self.iconbitmap(r'.\logo\cat_face_icon.ico')
+        #Se inicializan los estilos
+        styles = get_app_style()
+        self.title_screen()
+        self.camera_button()
+        self.search_file()
+        self.exit_button()
+    
+    def title_screen(self):
+        self.logo = tk.PhotoImage(file=r'.\logo\cat_face.png')
+        ttk.Label(self, 
+                text='Identificador de gatos', 
+                image=self.logo,
+                compound='left',
+                font=('Inter Bold',20)).place(x=45, y=50)
+    
+    def camera_button(self):
+        self.camera_icon = tk.PhotoImage(file=r'.\button_icon\camera_icon.png')
+        self.camera = ttk.Button(self,
+                                text= 'Cámara',
+                                style='EstiloBotones.TButton',
+                                image=self.camera_icon,
+                                compound='left',
+                                command=self.camera_cv2)
+        self.camera.place(x=45, y=200)
+    
+    def search_file(self):
+        self.search_icon = tk.PhotoImage(file=r'.\button_icon\search.png')
+        self.search = ttk.Button(self,
+                                 text='Buscar un vídeo o imagen',
+                                 style='EstiloBotones.TButton',
+                                 image=self.search_icon,
+                                 compound='left',
+                                 command=self.search_function)
+        self.search.place(x=42, y=270)
+        
+    def search_function(self):
+        self.archivo_seleccionado = filedialog.askopenfilename(
+            filetypes=[('Imagenes', '*.png; *.jpg; *.jpeg'),
+                       ('Archivos de Video', '*.mp4; *.avi')])
+        
+
+    def exit_button(self):
+        self.exit_icon = tk.PhotoImage(file=r'.\button_icon\exit.png')
+        self.exit = ttk.Button(text='Salir',
+                                style= 'EstiloBotones.TButton',
+                                image= self.exit_icon,
+                                compound='left',
+                                command= lambda: self.destroy())
+        self.exit.place(x=42, y=350)
+
+    def camera_cv2(self):
+        try:
+            self.withdraw()
+            camera_screen = CameraScreen(self)
+            camera_screen.mainloop()
+            self.deiconify()
+        except _tkinter.TclError as wm_command:
+            print(wm_command)
+
+if __name__ == '__main__':
+    InitialScreen().mainloop()
--- app.py
+++ app.py
@@ -1,68 +1,39 @@
+import cv2
 import tkinter as tk
-from tkinter import ttk
-from styles import get_app_style
-import os
-from tkinter import filedialog
+from tkinter import messagebox
 
-class InitialScreen(tk.Tk):
-    def __init__(self):
-        super().__init__()
-        #Cofiguracion de la pantalla
-        self.title('Pantalla de Inicio')
+class CameraScreen(tk.Toplevel):
+    def __init__(self, master= None):
+        super().__init__(master)
+        self.title('Cámara')
         self.geometry('1280x720')
         self.resizable(width=False, height=False)
-        #Se inicializan los estilos
-        styles = get_app_style()
-        self.title_screen()
-        self.camera_button()
-        self.search_file()
-        self.exit_button()
-    
-    def title_screen(self):
-        self.logo = tk.PhotoImage(file=r'.\logo\cat_face.png')
-        ttk.Label(self, 
-                text='Identificador de gatos', 
-                image=self.logo,
-                compound='left',
-                font=('Inter Bold',20)).pack()
-    
-    def camera_button(self):
-        self.camera_icon = tk.PhotoImage(file=r'.\button_icon\camera_icon.png')
-        self.camera = ttk.Button(self,
-                                text= 'Cámara',
-                                style='EstiloBotones.TButton',
-                                image=self.camera_icon,
-                                compound='left',
-                                command=self.camera_cv2)
-        self.camera.pack()
-    
-    def search_file(self):
-        self.search_icon = tk.PhotoImage(file=r'.\button_icon\search.png')
-        self.search = ttk.Button(self,
-                                 text='Buscar un vídeo o imagen',
-                                 style='EstiloBotones.TButton',
-                                 image=self.search_icon,
-                                 compound='left',
-                                 command=self.search_function)
-        self.search.pack()
-        
-    def search_function(self):
-        self.archivo_seleccionado = filedialog.askopenfilename(
-            filetypes=[('Imagenes', '*.png; *.jpg; *.jpeg'),
-                       ('Archivos de Video', '*.mp4; *.avi')])
-        
+        self.iconbitmap(r'.\logo\cat_face_icon.ico')
+        self.camara()
 
-    def exit_button(self):
-        self.exit_icon = tk.PhotoImage(file=r'.\button_icon\exit.png')
-        self.exit = ttk.Button(text='Salir',
-                                style= 'EstiloBotones.TButton',
-                                image= self.exit_icon,
-                                compound='left',
-                                command= lambda: self.destroy())
-        self.exit.pack()
+    def camara(self):
+        capture = cv2.VideoCapture(0)
+        if capture.isOpened() is False:
+            messagebox.showerror('Mensaje de error', 'Error al abrir la cámara.')
+            self.deiconify()
+            self.destroy()
+            return 
+        frame_index = 0
+        while capture.isOpened():
+            ret, frame = capture.read()
+            if ret is True:
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                img = tk.PhotoImage(data=frame.tobytes(), format='PPM')
+                if hasattr(self, 'label'):
+                    self.label.config(image=img)
+                    self.label.image = img
+                else:
+                    self.label = tk.Label(self, image=img)
+                    self.label.pack()
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+        capture.release()
+        cv2.destroyAllWindows()
 
-    def camera_cv2(self):
-        pass
+                
 
-if __name__ == '__main__':
-    InitialScreen().mainloop()
