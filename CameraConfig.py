6f5a2d464183f78ae987d0a7044439b81e9fbf27
Cristhian Arroyo <arroyocristhian8@gmail.com>
2023-10-08 13:45:04 -0500

    Arreglo de cámara, se ve el display en el centro de la pantalla.

--- CameraConfig.py
+++ CameraConfig.py
@@ -1,39 +1,41 @@
 import cv2
 import tkinter as tk
 from tkinter import messagebox
+from PIL import Image, ImageTk
 
 class CameraScreen(tk.Toplevel):
-    def __init__(self, master= None):
+    def __init__(self, master=None):
         super().__init__(master)
         self.title('Cámara')
         self.geometry('1280x720')
         self.resizable(width=False, height=False)
         self.iconbitmap(r'.\logo\cat_face_icon.ico')
+        self.capture = None
+        self.label = tk.Label(self)
+        self.label.pack(fill=tk.BOTH, expand=True)
         self.camara()
 
     def camara(self):
-        capture = cv2.VideoCapture(0)
-        if capture.isOpened() is False:
+        self.capture = cv2.VideoCapture(0)
+        if not self.capture.isOpened():
             messagebox.showerror('Mensaje de error', 'Error al abrir la cámara.')
-            self.deiconify()
             self.destroy()
-            return 
-
-        while capture.isOpened():
-            ret, frame = capture.read()
-            if ret is True:
-                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                img = tk.PhotoImage(data=frame.tobytes(), format='PPM')
-                if hasattr(self, 'label'):
-                    self.label.config(image=img)
-                    self.label.image = img
-                else:
-                    self.label = tk.Label(self, image=img)
-                    self.label.pack()
-                if cv2.waitKey(1) & 0xFF == ord('q'):
-                    break
-        capture.release()
-        cv2.destroyAllWindows()
+            return
+        self.update_camera()
+    
+    def update_camera(self):
+        ret, frame = self.capture.read()
+        if ret:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            img = ImageTk.PhotoImage(image=Image.fromarray(frame))
+            self.label.config(image=img)
+            self.label.image = img
+            self.after(10, self.update_camera)
+    
+    def __del__(self):
+        if self.capture is not None:
+            self.capture.release()
+            cv2.destroyAllWindows()
 
                 
 
